bcce01b10989c444d4d72130fd55d044df538d2d
Monica Ravipudi <monicaravipudi.iitkgp@gmail.com>
2024-03-21 10:48:55 -0400

    Update streamlit_app.py
    fruityvice to return info on selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # st.title("My Parents new Healthy diner")
 # Write directly to the app
@@ -26,17 +27,20 @@ ingredients_list = st.multiselect('Choose upto 5 ingredients:',
                                   max_selections = 5)
 
 if ingredients_list:
-    st.write(ingredients_list)
+    # st.write(ingredients_list)
     st.text(ingredients_list)
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
+    # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order+ """')"""
 
-    st.write(my_insert_stmt)
+    # st.write(my_insert_stmt)
     # st.stop()
 
     #submit order
@@ -49,7 +53,3 @@ if ingredients_list:
     #     session.sql(my_insert_stmt).collect()
     #     st.success('Your Smoothie is ordered!', icon="✅")
         
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
